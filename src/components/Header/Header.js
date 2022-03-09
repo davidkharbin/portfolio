c6db4729b7c0c7b8452c9aea9b8f2f783fc848c2
davidkharbin <david_k_harbin@hotmail.com>
2022-03-09 11:07:09 -0700

    feat: social icons and links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, anchor } from './HeaderStyles';
@@ -32,6 +32,17 @@ const Header = () => (
         </Link>
       </li>
     </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com/davidkharbin'>
+        <AiFillGithub size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/david-k-harbin/'>
+        <AiFillLinkedin size='3rem' />
+      </SocialIcons>
+      <SocialIcons href='https://twitter.com/davidkharbin'>
+        <AiFillTwitterCircle size='3rem' />
+      </SocialIcons>
+    </Div3>
   </Container >
 );
 
